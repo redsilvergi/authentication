47cb429cb2b14c25c3c253d92aebc4adafc3d506
silver <eungi429@gmail.com>
2023-05-15 08:12:42 +0900

    level 6 authentication - google oauth2.0

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}`));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,24 +33,77 @@ mongoose.connect("mongodb://127.0.0.1:27017/secretsUserDB");
 // mongoose.set("useCreateIndex", true)
 
 const userSchema = mongoose.Schema({
-  email: String,
+  username: String,
   password: String,
+  googleId: String,
+  googleDisplayName: String,
+  googleEmail: String,
 });
 
 //passport setup 3
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 //passport setup 4
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .exec()
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    (accessToken, refreshToken, fetchedData, cb) => {
+      // console.log(fetchedData);
+
+      User.findOrCreate(
+        {
+          googleId: fetchedData.id,
+          googleDisplayName: fetchedData.displayName,
+          googleEmail: fetchedData._json.email,
+        },
+        (err, user) => {
+          return cb(err, user);
+        }
+      );
+    }
+  )
+);
 
 app.get("/", (req, res) => {
   console.log(req.user);
   res.render("home");
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/register", (req, res) => {
   res.render("register");
@@ -60,6 +115,7 @@ app.get("/login", (req, res) => {
 
 app.get("/secrets", (req, res) => {
   if (req.isAuthenticated()) {
+    console.log(`secrets log: ${req.user}`);
     res.render("secrets", { user: req.user });
   } else {
     res.redirect("/login");
